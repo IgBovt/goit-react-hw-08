5b83684fdb9c79a5f9fc40d1bd36e15c8f5d769e
IgBovt <140014814+IgBovt@users.noreply.github.com>
2024-04-04 19:07:57 +0300

    upd home

--- src/components/Greeting/Greeting.jsx
+++ src/components/Greeting/Greeting.jsx
@@ -1,4 +1,5 @@
 import css from './Greeting.module.css';
+import img from '../../img/img.jpg';
 
 export default function Greeting() {
   return (
@@ -9,27 +10,20 @@ export default function Greeting() {
           💁‍♀️
         </span>
       </div>
-
-      <p className={css.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
-        asperiores eius nobis saepe quis voluptatibus facere expedita tempora,
-        hic similique natus minima fugit doloribus molestiae dolorum sed debitis
-        tempore consectetur repellat, accusantium aspernatur incidunt
-        doloremque. Quod doloribus beatae, molestiae voluptatibus, numquam
-        recusandae accusamus voluptatem aliquam, sed vero consectetur esse
-        harum?
-      </p>
-      <p className={css.text}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum et
-        illo non vel rerum odit, maxime, vitae dolorem molestiae error
-        cupiditate! Quasi eaque est provident voluptates adipisci? Cumque
-        quisquam laboriosam ullam, inventore ipsa ducimus quaerat dolorum natus
-        quasi amet ab earum vero, laborum cum quibusdam qui? Illo ab facere a
-        dolore, labore quisquam modi voluptatum esse nisi accusantium architecto
-        qui veritatis molestiae doloribus quas placeat, suscipit asperiores
-        fugiat quam sint perspiciatis ducimus! Officiis quam vel inventore fuga
-        dolorum, voluptatibus possimus.
-      </p>
+      <div className={css.container}>
+        <p className={css.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+          asperiores eius nobis saepe quis voluptatibus facere expedita tempora,
+          hic similique natus minima fugit doloribus molestiae dolorum sed
+          debitis tempore consectetur repellat, accusantium aspernatur incidunt
+          doloremque. Quod doloribus beatae, molestiae voluptatibus, numquam
+          recusandae accusamus voluptatem aliquam, sed vero consectetur esse
+          harum? Quod doloribus beatae, molestiae voluptatibus, numquam
+          recusandae accusamus voluptatem aliquam, sed vero consectetur esse
+          harum?
+        </p>
+        <img src={img} alt="Phonebook" width={500} height={400} />
+      </div>
     </div>
   );
 }
