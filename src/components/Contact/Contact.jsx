995e98767d9119624825af9a7a0f5c0fc0531874
IgBovt <140014814+IgBovt@users.noreply.github.com>
2024-04-05 00:58:13 +0300

    upd contact menu

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,49 +1,65 @@
 import { deleteContact } from '../../redux/contacts/operations';
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { FaUserAlt } from 'react-icons/fa';
 import { FaPhoneAlt } from 'react-icons/fa';
 import { FaPhoneVolume } from 'react-icons/fa';
-import css from './Contact.module.css';
 import { deleteToast, deleteErrToast } from '../../js/toasts';
+import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
+import css from './Contact.module.css';
 
 export default function Contact({ id, name, number }) {
   const dispatch = useDispatch();
+  const [showBtn, setShowBtn] = useState(false);
+  const handleEditClick = () => {
+    setShowBtn(!showBtn);
+  };
   return (
     <div className={css.container}>
-      <div className={css.phoneContainer}>
-        <div>
-          <p>
-            <FaUserAlt className={css.icon} size={14} />
-            {name}
-          </p>
-          <p>
-            <FaPhoneAlt className={css.icon} size={14} />
-            {number}
-          </p>
-        </div>
-
-        <a className={css.phoneLink} href={`tel:${number}`}>
-          <FaPhoneVolume className={css.phoneIcon} size={20} />
-        </a>
+      {/* <div className={css.phoneContainer}> */}
+      <div>
+        <p>
+          <FaUserAlt className={css.icon} size={14} />
+          {name}
+        </p>
+        <p>
+          <FaPhoneAlt className={css.icon} size={14} />
+          {number}
+        </p>
       </div>
-      <hr className={css.line} />
-      <div className={css.btnContainer}>
-        <button className={css.btnEdit}>Edit</button>
+
+      <a className={css.phoneLink} href={`tel:${number}`}>
+        <FaPhoneVolume className={css.phoneIcon} size={20} />
+      </a>
+      {/* </div> */}
+      <div className={css.wrapper}>
         <button
-          className={css.btn}
-          onClick={() => {
-            dispatch(deleteContact(id))
-              .unwrap()
-              .then(() => {
-                deleteToast(name);
-              })
-              .catch(() => {
-                deleteErrToast();
-              });
-          }}
+          className={css.btnArrow}
+          type="button"
+          onClick={handleEditClick}
         >
-          Delete
+          {showBtn ? <IoIosArrowUp /> : <IoIosArrowDown />}
         </button>
+        {showBtn && (
+          <div className={css.btnContainer}>
+            <button className={css.btn}>Edit</button>
+            <button
+              className={css.btn}
+              onClick={() => {
+                dispatch(deleteContact(id))
+                  .unwrap()
+                  .then(() => {
+                    deleteToast(name);
+                  })
+                  .catch(() => {
+                    deleteErrToast();
+                  });
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
